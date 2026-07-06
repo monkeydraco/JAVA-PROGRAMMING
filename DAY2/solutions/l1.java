ab732599e3be88e5eb10d48ed3e5aaa697c12915
monkeydraco <chiefdraco101@gmail.com>
2026-07-06 23:59:44 +0530

    DAY5 commit 2

---
+++ DAY2/solutions/l1.java
@@ -0,0 +1,21 @@
+package DAY2.solutions;
+
+public class l1 
+{
+    public static void main(String[] args)
+    {
+        String name="Sanjana";
+        int age = 19;
+        String usn = "2025CS0077";
+        String branch = "CSE";
+        float cgpa=9.5f;
+
+        System.out.println("Name : "+name);
+        System.out.println("Age : " + age);
+        System.out.println("USN : "+usn);
+        System.out.println("Branch : "+branch);
+        System.out.println("CGPA : "+cgpa);
+
+    }
+    
+}
