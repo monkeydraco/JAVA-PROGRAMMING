5ac6960263a5ad9fcb48644b06dc63d5f141fc0a
monkeydraco <chiefdraco101@gmail.com>
2026-07-07 00:14:07 +0530

    DAY6 commit 1

---
+++ DAY2/solutions/l12.java
@@ -0,0 +1,17 @@
+package DAY2.solutions;
+
+public class l12
+{
+    public static void main()
+    {
+        int a=10;
+        int b=20;
+
+        System.out.println("SUM : "+ (a+b));
+        System.out.println("DIFFERENCE : "+(a-b));
+        System.out.println("PRODUCT : "+(a*b));
+        System.out.println("DIVISION : "+(a/b));
+        System.out.println("REMAINDER : "+(a%b));
+
+    }
+}
---
+++ DAY2/solutions/l13.java
@@ -0,0 +1,18 @@
+package DAY2.solutions;
+
+public class l13 
+{
+    public static void main()
+    {
+        float length;
+        float  breadth;
+        length=10f;
+        breadth = 50f;
+
+        float area=length*breadth;
+
+        System.out.println("AREA : "+area);
+
+    }
+    
+}
